b73e606ac45a73e015e2a384c6f413be88d9d06f
Denwa799 <denwa799@yandex.ru>
2022-07-03 15:53:36 +0300

    Frontend Первый коммит

---
+++ client/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+import { Main } from '@components/Main';
+
+const App = () => {
+  return (
+    <View>
+      <Text>React</Text>
+      <Main/>
+    </View>
+  );
+};
+
+export default App;
---
+++ client/src/components/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+export const Main = () => {
+  return (
+    <SafeAreaView>
+      <Text>main component</Text>
+    </SafeAreaView>
+  );
+};
