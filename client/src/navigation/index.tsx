ad2664825b01d989c80f2b54a39112f00b62d230
Denwa799 <denwa799@yandex.ru>
2022-12-26 03:00:39 +0300

    Frontend улучшает анимацию bottom tab и убирает рамки у шапки навигатора

--- client/src/navigation/index.tsx
+++ client/src/navigation/index.tsx
@@ -5,7 +5,10 @@ import {THEME} from 'constants/theme';
 
 export const AppNavigation = () => {
   return (
-    <NavigationContainer theme={{colors: {background: THEME.BACK_COLOR}}}>
+    <NavigationContainer
+      theme={{
+        colors: {background: THEME.BACK_COLOR, border: THEME.BACK_COLOR},
+      }}>
       <AuthNavigation />
     </NavigationContainer>
   );
